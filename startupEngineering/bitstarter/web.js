78026f1619141b609b696e52e998518cb63c13f2
mtnguyen <minh.9.nguyen@gmail.com>
2013-07-07 16:31:37 -0400

    display html using express

--- startupEngineering/bitstarter/web.js
+++ startupEngineering/bitstarter/web.js
@@ -1,14 +1,30 @@
+var express = require('express');
 var http = require('http'),
     fs = require('fs');
 
+var app = express.createServer(express.logger());
+app.set("view options", {layout: false});
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(5000);
+app.configure(function(){
+  // disable layout
+  app.set("view options", {layout: false});
+
+  // make a custom html template
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
 });
+
+app.get('/', function(req, res){
+  res.render("index.html");
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
